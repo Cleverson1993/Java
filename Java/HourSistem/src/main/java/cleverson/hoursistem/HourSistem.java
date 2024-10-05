e142e06b5b162d2a20167575d430207dfba81215
cleverson <cleversonpereir4@gmail.com>
2024-10-05 20:58:19 -0300

    Add files via upload

---
+++ Java/HourSistem/src/main/java/cleverson/hoursistem/HourSistem.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cleverson.hoursistem;
+
+import java.util.Date;
+
+/**
+ *
+ * @author cleve
+ */
+public class HourSistem {
+
+    public static void main(String[] args) {
+        
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é");
+        System.out.println(relogio.toString());
+    }
+}
